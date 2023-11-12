66445ca1edcc0cf2aafb54336a00adf2643a5d6c
WEI QI <weiqi5588@gmail.com>
2023-11-12 18:54:12 -0500

    add round and abs method
    add round and abs method

--- D2/METRICSTICS/statistic_tool/calculator.py
+++ D2/METRICSTICS/statistic_tool/calculator.py
@@ -23,7 +23,7 @@ class Calculator:
         for value in self.data:
             if value < min_value:
                 min_value = value
-        return round(min_value, 2)
+        return self.round_number(min_value, 2)
 
     def maximum(self):
         if not self.data:
@@ -33,7 +33,7 @@ class Calculator:
         for value in self.data:
             if value > max_value:
                 max_value = value
-        return round(max_value, 2)
+        return self.round_number(max_value, 2)
 
     def mode(self):
         if not self.data:
@@ -67,18 +67,18 @@ class Calculator:
             return None
         n = len(sorted_data)
         if n % 2 == 1:
-            return round(sorted_data[n // 2], 2)
+            return self.round_number(sorted_data[n // 2], 2)
         else:
             mid1 = sorted_data[(n - 1) // 2]
             mid2 = sorted_data[n // 2]
-            return round((mid1 + mid2) / 2, 2)
+            return self.round_number((mid1 + mid2) / 2, 2)
 
     def mean(self):
         if not self.data:
             print("Error: No data available. Please input data first.")
             return None
         try:
-            return round(sum(self.data) / len(self.data), 2)
+            return self.round_number(sum(self.data) / len(self.data), 2)
         except ZeroDivisionError:
             print("Error: Division by zero occurred. This should not happen.")
             return None
@@ -90,7 +90,7 @@ class Calculator:
         μ = self.mean()
         if μ is None:
             return None
-        return round(sum(abs(x - μ) for x in self.data) / len(self.data), 2)
+        return self.round_number(sum(self.absolute_value(x - μ) for x in self.data) / len(self.data), 2)
 
     def standard_deviation(self, decimal_places=2):
         if not self.data:
@@ -101,7 +101,7 @@ class Calculator:
             return None
         variance = sum((x - μ) ** 2 for x in self.data) / len(self.data)
         std_dev = variance ** 0.5
-        return round(std_dev, decimal_places)
+        return self.round_number(std_dev, decimal_places)
 
     def descriptive_statistics(self):
         if not self.data:
@@ -116,3 +116,38 @@ class Calculator:
             "Mean Absolute Deviation": self.mean_absolute_deviation(),
             "Standard Deviation": self.standard_deviation()
         }
+
+    def round_number(self, number, ndigits=None):
+        if ndigits is None:
+            ndigits = 0
+        elif ndigits < 0:
+            raise ValueError("ndigits must be non-negative")
+
+        # Shift the decimal point to the right by ndigits
+        # So we can work with the integer part for rounding
+        shift = 10 ** ndigits
+        temp = number * shift
+
+        # Get the integer and the fractional part of the number
+        integer_part = int(temp)
+        fractional_part = temp - integer_part
+
+        # Check the fractional part to decide how to round the integer part
+        if fractional_part >= 0.5:
+            # Round up
+            integer_part += 1
+
+        # Shift the decimal point back to the original position
+        return integer_part / shift
+
+    def absolute_value(self, number):
+        """
+        Return the absolute value of the number given.
+        """
+        # Check if the number is negative
+        if number < 0:
+            # If the number is negative, make it positive by multiplying by -1
+            return -number
+        else:
+            # If the number is not negative, it is already positive, so return as is
+            return number
