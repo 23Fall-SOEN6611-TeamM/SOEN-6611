e8cacfc92e52a8f610ac46fb06b971ad107eb97d
vinay <vinaysahrawat183a@gmail.com>
2023-11-21 22:12:40 -0500

    added code to install missing dependencies if not already installed

--- D2/METRICSTICS/main.py
+++ D2/METRICSTICS/main.py
@@ -1,3 +1,19 @@
+import subprocess
+
+def install_if_not_exists(package):
+    try:
+        __import__(package)
+    except ImportError:
+        print(f"Installing {package}...")
+        subprocess.check_call(['pip', 'install', package])
+
+# List your dependencies
+dependencies = ['requests', 'numpy', 'pandas', 'matplotlib', 'scikit-learn', 'openpyxl']
+
+# Install missing dependencies
+for dependency in dependencies:
+    install_if_not_exists(dependency)
+    
 from ui_element.metricstics_app import MetricsticsApp
 
 if __name__ == "__main__":
